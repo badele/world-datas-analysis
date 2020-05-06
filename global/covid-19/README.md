f8883f3766cd29ee877ab536ac5e968ff68d3bf1
Bruno Adelé <bruno@adele.im>
2020-05-06 14:39:45 +0200

    Refactoring with sqlite common database support

---
+++ global/covid-19/README.md
@@ -0,0 +1,87 @@
+# Covid-19 Summaries
+
+## Download JHU datas
+```
+python global/covid-19/download_from_jhu.py
+sqlite3 world-datas-analysis.db < import_global_covid19_jhu.sql
+```
+# Graph datas
+
+### Cases
+```
+FORHAB=1000000
+FIELD="ratio_cases"
+SOURCE="https://github.com/CSSEGISandData/COVID-19"
+COUNTRIES="'Spain','US','Italy','United Kingdom','France','Germany','Korea South','Sweden'"
+QUERY="SELECT * FROM v_global_covid19_JHU WHERE zone='country' AND country_region IN ($COUNTRIES)"
+python sql_to_gnuplot.py --db world-datas-analysis.db \
+--query "$QUERY" --block-by country_region --comment "Source: $SOURCE" \
+--output global/covid-19/datas/countries_${FIELD}_for_${FORHAB}hab.gdata
+
+gnuplot -e "field='${FIELD}';forhab=${FORHAB}" gnuplot/color_grafana.plt global/covid-19/gnuplot/countries.gp
+```
+
+<img width="50%" height="50%" src="pictures/countries_ratio_cases_for_1000000hab.png"/>
+
+[Gnuplot Datafile source](datas/countries_ratio_cases_for_1000000hab.gdata)
+
+
+### Deaths
+```
+FORHAB=1000000
+FIELD="ratio_deaths"
+SOURCE="https://github.com/CSSEGISandData/COVID-19"
+COUNTRIES="'Spain','US','Italy','United Kingdom','France','Germany','Korea South','Sweden'"
+QUERY="SELECT * FROM v_global_covid19_JHU WHERE zone='country' AND country_region IN ($COUNTRIES)"
+python sql_to_gnuplot.py --db world-datas-analysis.db \
+--query "$QUERY" --block-by country_region --comment "Source: $SOURCE" \
+--output global/covid-19/datas/countries_${FIELD}_for_${FORHAB}hab.gdata
+
+gnuplot -e "field='${FIELD}';forhab=${FORHAB}" gnuplot/color_grafana.plt global/covid-19/gnuplot/countries.gp
+```
+
+<img width="50%" height="50%" src="pictures/countries_ratio_deaths_for_1000000hab.png"/>
+
+[Gnuplot Datafile source](datas/countries_ratio_deaths_for_1000000hab.gdata)
+
+
+# Filter value datas
+
+### Cases
+```
+NBVALUE=1
+FORHAB=1000000
+FIELD="ratio_cases"
+SOURCE="https://github.com/CSSEGISandData/COVID-19"
+COUNTRIES="'Spain','US','Italy','United Kingdom','France','Germany','Korea South','Sweden'"
+QUERY="SELECT * FROM v_global_covid19_JHU WHERE ${FIELD}>(${NBVALUE}.0/${FORHAB}) AND zone='country' AND country_region IN ($COUNTRIES)"
+python sql_to_gnuplot.py --db world-datas-analysis.db \
+--query "$QUERY" --block-by country_region --comment "Source: $SOURCE" \
+--output global/covid-19/datas/countries_${FIELD}_filter_${NBVALUE}_for_${FORHAB}hab.gdata
+
+gnuplot -e "field='${FIELD}';nbvalue=${NBVALUE};forhab=${FORHAB}" gnuplot/color_grafana.plt global/covid-19/gnuplot/countries_without_axis_date.gp
+```
+
+<img width="50%" height="50%" src="pictures/countries_ratio_cases_filter_1_for_1000000hab.png"/>
+
+[Gnuplot Datafile source](datas/countries_ratio_cases_filter_1_for_1000000hab.gdata)
+
+
+### Deaths
+```
+NBVALUE=1
+FORHAB=1000000
+FIELD="ratio_deaths"
+SOURCE="https://github.com/CSSEGISandData/COVID-19"
+COUNTRIES="'Spain','US','Italy','United Kingdom','France','Germany','Korea South','Sweden'"
+QUERY="SELECT * FROM v_global_covid19_JHU WHERE ${FIELD}>(${NBVALUE}.0/${FORHAB}) AND zone='country' AND country_region IN ($COUNTRIES)"
+python sql_to_gnuplot.py --db world-datas-analysis.db \
+--query "$QUERY" --block-by country_region --comment "Source: $SOURCE" \
+--output global/covid-19/datas/countries_${FIELD}_filter_${NBVALUE}_for_${FORHAB}hab.gdata
+
+gnuplot -e "field='${FIELD}';nbvalue=${NBVALUE};forhab=${FORHAB}" gnuplot/color_grafana.plt global/covid-19/gnuplot/countries_without_axis_date.gp
+```
+
+<img width="50%" height="50%" src="pictures/countries_ratio_deaths_filter_1_for_1000000hab.png"/>
+
+[Gnuplot Datafile source](datas/countries_ratio_deaths_filter_1_for_1000000hab.gdata)
--- global/covid-19/download_from_jhu.py
+++ global/covid-19/download_from_jhu.py
@@ -3,20 +3,12 @@ import os
 
 import argparse
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-
-# Argument options
-ap = argparse.ArgumentParser()
-ap.add_argument("--output", type=str,
-                help="output filename")
-args = vars(ap.parse_args())
+# Download Covid-19 datas from https://github.com/CSSEGISandData/COVID-19
 
 COLUMNS = ['cases', 'recovered', 'deaths']
 
-
-def loadData(field, url):
+def loadAndExportData(field, url):
     df = pd.read_csv(url)
 
     df = df.rename(columns={
@@ -27,15 +19,17 @@ def loadData(field, url):
 
     df = df.melt(id_vars=["Province_State", "Country_Region", "Lat", "Long"],
                  var_name="date",
-                 value_name=field)
+                 value_name='value')
 
-    # Country & State
-    iscountry = df['Province_State'].isnull()
-    df.loc[iscountry, 'zone'] = 'country'
-    df.loc[~iscountry, 'zone'] = 'state'
+    df["field"] = field
+    df['date'] = pd.to_datetime(df['date'])
 
-    # Merge country and state
-    df = df.set_index(['Country_Region', 'Province_State', 'date'])
+    df = df[["date", "Country_Region", "Province_State",  "field", "value"]]
+
+    df.to_csv(
+        f'global/covid-19/datas/global_covid19_JHU_{field}_downloaded.csv',
+        index=False
+    )
 
     return df
 
@@ -48,73 +42,24 @@ population = pd.read_csv(
 population = population.rename(columns={
     'Population': 'population',
 })
-
-# Create countries from population data
-countries = population[population['Province_State'].isnull()]
-countries = countries.set_index('Country_Region')
-countries['zone'] = 'country'
-
-# Create provinces from population data
-provinces = population[~population['Province_State'].isnull()]
-provinces = provinces.set_index('Province_State')
-provinces['zone'] = 'state'
-
-countries = countries.drop(['Long_'], axis=1)
-provinces = provinces.drop(['Long_'], axis=1)
-
+# Save JHU datas files
+population.to_csv(
+    'global/covid-19/datas/global_covid19_JHU_locations_downloaded.csv',
+    index=False
+)
 
 #######################################
-# Datas
+# Load and export cases,recovered,deaths Datas
 #######################################
-cases = loadData(
+cases = loadAndExportData(
     'cases',
     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 )
-recovered = loadData(
+recovered = loadAndExportData(
     'recovered',
     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
 )
-deaths = loadData(
+deaths = loadAndExportData(
     'deaths',
     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
 )
-
-
-# Merge column
-df = cases.copy()
-df['recovered'] = recovered['recovered']
-df['deaths'] = deaths['deaths']
-
-# Update columns
-#df = df.drop(['Lat', 'Long'], axis=1)
-df[COLUMNS] = df[COLUMNS].fillna(0)
-df[COLUMNS] = df[COLUMNS].astype(int)
-
-df = df.reset_index()
-df = pd.merge(
-    df, population,
-    left_on=['Province_State', 'Country_Region'],
-    right_on=['Province_State', 'Country_Region']
-)
-
-# Add/Update column informations
-for column in COLUMNS:
-    df[f'pct_{column}'] = (
-        df[column] / df['population']*100).astype(float)
-
-# Convert date
-df['date'] = pd.to_datetime(df['date'])
-
-df = df.drop(['Long_', 'Combined_Key', 'Lat_y'], axis=1)
-df = df.rename({'Lat_x': 'Lat'}, axis=1)
-
-# Order column
-df = df[['date', 'cases', 'recovered', 'deaths', 'pct_cases', 'pct_recovered', 'pct_deaths', 'Country_Region', 'Province_State',
-         'zone', 'UID', 'iso2', 'iso3', 'FIPS', 'Admin2', 'Lat', 'Long', 'population']]
-
-# Sort
-df = df.sort_values(['Country_Region', 'Province_State', 'date'])
-
-# Save file
-df.to_csv(args['output'],  sep=",", index=False,
-          index_label="idx", float_format='%.6f')
