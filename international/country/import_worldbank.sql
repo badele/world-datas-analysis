894f943adc9b58c27e24365be9a4de01233523f2
Bruno Adelé <brunoadele@gmail.com>
2021-09-25 14:01:20 +0200

    Add worldbank indicators

--- international/country/import_worldbank.sql
+++ international/country/import_worldbank.sql
@@ -3,30 +3,13 @@
 
 BEGIN TRANSACTION;
 
---------------------------------------
--- Import indicator
---------------------------------------
-
-DROP TABLE IF EXISTS "worldbank_indicator";
-CREATE TABLE "worldbank_indicator" (
-  "INDICATOR_CODE" TEXT,
-  "INDICATOR_NAME" TEXT,
-  "SOURCE_NOTE" TEXT,
-  "SOURCE_ORGANIZATION" TEXT,
-
-  PRIMARY KEY (INDICATOR_CODE)
-);
-
-INSERT INTO worldbank_indicator
-SELECT * FROM x_downloaded_worldbank_indicator;
-
 
 --------------------------------------
 -- Import country
 --------------------------------------
 
-DROP TABLE IF EXISTS "worldbank_country";
-CREATE TABLE "worldbank_country" (
+DROP TABLE IF EXISTS worldbank_country;
+CREATE TABLE worldbank_country (
   CountryCode TEXT,
   IndicatorCode TEXT,
   "year" INTEGER,
@@ -38,413 +21,119 @@ CREATE TABLE "worldbank_country" (
 INSERT INTO worldbank_country
 SELECT * FROM x_downloaded_worldbank_country where value is not NULL;
 
---------------------------------------
--- Import country
---------------------------------------
-
-DROP TABLE IF EXISTS "worldbank_country_indicators_summary";
-CREATE TABLE "worldbank_country_indicators_summary" (
-  CountryCode TEXT,
-  IndicatorCode TEXT,
-  "nb_years" INTEGER,
-  "data_quality" REAL,
-  "year_first" INTEGER,
-  "year_last" INTEGER,
-  "value_first" REAL,
-  "value_min" REAL,
-  "value_avg" REAL,
-  "value_max" REAL,
-  "value_last" REAL,
-  "trend" REAL,
-  "slope" REAL,
-
-  PRIMARY KEY (CountryCode,IndicatorCode)
-);
-
-
-INSERT INTO worldbank_country_indicators_summary
-SELECT CountryCode, 
-IndicatorCode,
-COUNT(year) - 1, 
-ROUND(100.0*COUNT(year)/(CAST(strftime('%Y') AS decimal) - 1960.0),4) as nbyears, 
-MIN(year) year_first, 
-MAX(year) yaer_last, 
-NULL value_first,
-MIN(value) value_min, 
-AVG(value) value_avg, 
-MAX(value) value_max, 
-NULL value_last,
-NULL trend,
-ROUND((COUNT(value) * SUM(year * value) - (SUM(year) * SUM(value))) / ((COUNT(value) * SUM(year * year)) - (SUM(year) * SUM(year))),4) slope
-FROM worldbank_country wc
-GROUP BY CountryCode, IndicatorCode;
-
-
--- First
-UPDATE worldbank_country_indicators_summary as wu set value_first = (SELECT value
-FROM worldbank_country wc where (wu.CountryCode = wc.CountryCode AND wu.IndicatorCode = wc.IndicatorCode AND wu.year_first = wc.year));  
-
--- Last
-UPDATE worldbank_country_indicators_summary as wu set value_last = (SELECT value
-FROM worldbank_country wc where (wu.CountryCode = wc.CountryCode AND wu.IndicatorCode = wc.IndicatorCode AND wu.year_last = wc.year));  
-
--- Trend
-UPDATE worldbank_country_indicators_summary set trend = ROUND((value_last/value_first - 1) * 100,4);
-
 
 --------------------------------------
 -- all  period summaries
 --------------------------------------
 
-DROP TABLE IF EXISTS "worldbank_country_indicators_summary";
-CREATE TABLE "worldbank_country_indicators_summary" (
+DROP TABLE IF EXISTS worldbank_country_summary;
+CREATE TABLE worldbank_country_summary (
   CountryCode TEXT,
   IndicatorCode TEXT,
-  range TEXT,
-  period TEXT,
-  summary TEXT,
-  value REAL,
+  first_year INTEGER,
+  last_year INTEGER,
+  nbyears INTEGER,
+  first_value REAL,
+  last_value REAL,
+  growth_percent REAL,
+  min_value REAL,
+  max_value REAL,
+  lr_a REAL,
+  lr_b REAL,
+  lr_percent_slope REAL,
+  data_quality REAL,
+  corr REAL,
   
-  PRIMARY KEY (CountryCode,IndicatorCode,range,period,summary)
-);
-
--- nb years
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-CountryCode, 
-IndicatorCode, 
-'all' as range,
-'all' as period,
-'nb_years' as summary,
-COUNT(year) as value
-FROM worldbank_country
-GROUP BY CountryCode,IndicatorCode
-ORDER BY value DESC;
-
--- quality
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-wc.CountryCode, 
-wc.IndicatorCode, 
-'all' as range,
-'all' as period,
-'quality' as sumary,
-ROUND(100.0*COUNT(year)/(CAST(strftime('%Y') AS decimal) - 1960.0),4) as value
-FROM worldbank_country wc
-CROSS JOIN worldbank_country_indicators_summary cs ON 
-  wc.CountryCode = cs.CountryCode AND
-  wc.IndicatorCode = cs.IndicatorCode AND
-  cs.range='all' AND
-  cs.period='all' AND
-  cs.summary='nb_years'
-GROUP BY wc.CountryCode,wc.IndicatorCode
-ORDER BY value DESC;
-
--- min
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-CountryCode, 
-IndicatorCode, 
-'all' as range,
-'all' as period,
-'year_first' as sumary,
-min(year) as value
-FROM worldbank_country
-GROUP BY CountryCode,IndicatorCode
-ORDER BY value DESC;
-
--- max
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-CountryCode, 
-IndicatorCode, 
-'all' as range,
-'all' as period,
-'year_last' as sumary,
-max(year) as value
-FROM worldbank_country
-GROUP BY CountryCode,IndicatorCode
-ORDER BY value DESC;
-
--- value_first
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-wc.CountryCode, 
-wc.IndicatorCode, 
-'all' as range,
-'all' as period,
-'value_first' as sumary,
-wc.value as value
-FROM worldbank_country wc
-INNER JOIN worldbank_country_indicators_summary cs ON 
-  wc.CountryCode = cs.CountryCode AND
-  wc.IndicatorCode = cs.IndicatorCode AND
-  cs.range='all' AND
-  cs.period='all' AND
-  cs.summary='year_first'
-WHERE wc.year = cs.value
-GROUP BY wc.CountryCode,wc.IndicatorCode
-ORDER BY value DESC;
-
--- value_last
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-wc.CountryCode, 
-wc.IndicatorCode, 
-'all' as range,
-'all' as period,
-'value_last' as sumary,
-wc.value as value
-FROM worldbank_country wc
-INNER JOIN worldbank_country_indicators_summary cs ON 
-  wc.CountryCode = cs.CountryCode AND
-  wc.IndicatorCode = cs.IndicatorCode AND
-  cs.range='all' AND
-  cs.period='all' AND
-  cs.summary='year_last'
-WHERE wc.year = cs.value
-GROUP BY wc.CountryCode,wc.IndicatorCode
-ORDER BY value DESC;
-
--- linear regression a
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-CountryCode, 
-IndicatorCode, 
-'all' as range,
-'all' as period,
-'linearregression_a' as summary,
-ROUND((COUNT(value) * SUM(year * value) - (SUM(year) * SUM(value))) / ((COUNT(value) * SUM(year * year)) - (SUM(year) * SUM(year))),4) value
-FROM worldbank_country
-GROUP BY CountryCode,IndicatorCode
-ORDER BY value DESC;
-
--- linear regression b
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-wc.CountryCode, 
-wc.IndicatorCode, 
-'all' as range,
-'all' as period,
-'linearregression_b' as sumary,
-(SUM(wc.value) / COUNT(wc.value)) - cs.value * (SUM(wc.year) / COUNT(wc.year)) as value
-FROM worldbank_country wc
-INNER JOIN worldbank_country_indicators_summary cs ON 
-  wc.CountryCode = cs.CountryCode AND
-  wc.IndicatorCode = cs.IndicatorCode AND
-  cs.range='all' AND
-  cs.period='all' AND
-  cs.summary='linearregression_a'
-GROUP BY wc.CountryCode,wc.IndicatorCode
-ORDER BY value DESC;
-
--- linear regression b
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-wc.CountryCode, 
-wc.IndicatorCode, 
-'all' as range,
-'all' as period,
-'coef' as sumary,
-(SUM(wc.value) / COUNT(wc.value)) - cs.value * (SUM(wc.year) / COUNT(wc.year)) as value
-FROM worldbank_country wc
-INNER JOIN worldbank_country_indicators_summary cs ON 
-  wc.CountryCode = cs.CountryCode AND
-  wc.IndicatorCode = cs.IndicatorCode AND
-  cs.range='all' AND
-  cs.period='all' AND
-  cs.summary='linearregression_a'
-GROUP BY wc.CountryCode,wc.IndicatorCode
-ORDER BY value DESC;
-
--- coef
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-wc.CountryCode, 
-wc.IndicatorCode, 
-'all' as range,
-'all' as period,
-'corr' as summary,
-SUM((wc.year-agg.avg_year)*(wc.value-agg.avg_value)) /
-(SQRT(SUM(POW(wc.year,2))) * SQRT(SUM(POW(wc.value,2)))) as value
-FROM worldbank_country wc,
- (SELECT
-*, 
-AVG(value) as avg_value,
-AVG(year) as avg_year
-FROM worldbank_country wc
-GROUP BY CountryCode,IndicatorCode) as agg
-WHERE  wc.CountryCode = agg.CountryCode AND
-wc.IndicatorCode = agg.IndicatorCode
-GROUP BY wc.CountryCode,wc.IndicatorCode;
-
---------------------------------------
--- decade summaries
---------------------------------------
-
--- nb years
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-CountryCode, 
-IndicatorCode, 
-'decade' as range,
-year - (year % 10) as period,
-'nb_years' as summary,
-COUNT(year) as value
-FROM worldbank_country
-GROUP BY CountryCode,IndicatorCode,year - (year % 10)
-ORDER BY value DESC;
-
--- quality
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-wc.CountryCode, 
-wc.IndicatorCode, 
-'decade' as range,
-year - (year % 10) as period,
-'quality' as sumary,
-ROUND(100.0*COUNT(year)/(CAST(strftime('%Y') AS decimal) - 1960.0),4) as value
-FROM worldbank_country wc
-CROSS JOIN worldbank_country_indicators_summary cs ON 
-  wc.CountryCode = cs.CountryCode AND
-  wc.IndicatorCode = cs.IndicatorCode AND
-  cs.range='all' AND
-  cs.period='all' AND
-  cs.summary='nb_years'
-GROUP BY wc.CountryCode,wc.IndicatorCode,year - (year % 10)
-ORDER BY value DESC;
-
--- min
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-CountryCode, 
-IndicatorCode, 
-'decade' as range,
-year - (year % 10) as period,
-'year_first' as sumary,
-min(year) as value
-FROM worldbank_country
-GROUP BY CountryCode,IndicatorCode,year - (year % 10)
-ORDER BY value DESC;
-
--- max
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-CountryCode, 
-IndicatorCode, 
-'decade' as range,
-year - (year % 10) as period,
-'year_last' as sumary,
-max(year) as value
-FROM worldbank_country
-GROUP BY CountryCode,IndicatorCode,year - (year % 10)
-ORDER BY value DESC;
-
--- value_first
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-wc.CountryCode, 
-wc.IndicatorCode, 
-'decade' as range,
-year - (year % 10) as period,
-'value_first' as sumary,
-wc.value as value
-FROM worldbank_country wc
-INNER JOIN worldbank_country_indicators_summary cs ON 
-  wc.CountryCode = cs.CountryCode AND
-  wc.IndicatorCode = cs.IndicatorCode AND
-  cs.range='all' AND
-  cs.period='all' AND
-  cs.summary='year_first'
-WHERE wc.year = cs.value
-GROUP BY wc.CountryCode,wc.IndicatorCode,year - (year % 10)
-ORDER BY value DESC;
-
--- value_last
-INSERT INTO worldbank_country_indicators_summary
-SELECT 
-wc.CountryCode, 
-wc.IndicatorCode, 
-'decade' as range,
-year - (year % 10) as period,
-'value_last' as sumary,
-wc.value as value
-FROM worldbank_country wc
-INNER JOIN worldbank_country_indicators_summary cs ON 
-  wc.CountryCode = cs.CountryCode AND
-  wc.IndicatorCode = cs.IndicatorCode AND
-  cs.range='all' AND
-  cs.period='all' AND
-  cs.summary='year_last'
-WHERE wc.year = cs.value
-GROUP BY wc.CountryCode,wc.IndicatorCode,year - (year % 10)
-ORDER BY value DESC;
-
-DROP TABLE IF EXISTS "worldbank_country_category_summary";
-CREATE TABLE "worldbank_country_category_summary" (
-  CountryCode TEXT,
-  IndicatorCode TEXT,
-  "year_min" INTEGER,
-  "value_min" REAL,
-  "year_max" INTEGER,
-  "value_max" REAL,
-
   PRIMARY KEY (CountryCode,IndicatorCode)
 );
 
+INSERT INTO worldbank_country_summary
+SELECT 
+wc.CountryCode, 
+wc.IndicatorCode, 
+MIN(wc.year) AS first_year,
+MAX(wc.year) AS last_year,
+COUNT(wc.year) AS nbyears,
+NULL AS first_value,
+NULL AS last_value,
+NULL AS growth_percent,
+MIN(wc.value) AS min_value,
+MAX(wc.value) AS max_value,
+ROUND((COUNT(wc.value) * SUM(wc.year * wc.value) - (SUM(wc.year) * SUM(wc.value))) / ((COUNT(wc.value) * SUM(POW(wc.year,2))) - (SUM(wc.year) * SUM(wc.year))),4) AS lr_a,
+(SUM(wc.value) / COUNT(wc.value)) - (ROUND((COUNT(wc.value) * SUM(wc.year * wc.value) - (SUM(wc.year) * SUM(wc.value))) / ((COUNT(wc.value) * SUM(POW(wc.year,2))) - (SUM(wc.year) * SUM(wc.year))),4)) * (SUM(wc.year) / COUNT(wc.year)) AS lr_b,
+NULL AS lr_percent_slope,
+ROUND(COUNT(wc.year) / wc1.nb_years * 100,2) AS data_quality,
+SUM((wc.year-wc2.avg_year)*(wc.value-wc2.avg_value)) /
+(SQRT(SUM(POW(wc.year-wc2.avg_year,2))) * SQRT(SUM(POW(wc.value-wc2.avg_value,2)))) AS corr
+FROM worldbank_country wc,
+ (SELECT
+MAX(year) - MIN(year) + 1.0 nb_years, 
+MIN(year) min_year,
+MAX(year) max_year
+FROM worldbank_country) wc1,
+ (SELECT
+ *,
+AVG(value) AS avg_value,
+AVG(year) AS avg_year
+FROM worldbank_country
+GROUP BY CountryCode,IndicatorCode) AS wc2
+WHERE wc.CountryCode = wc2.CountryCode AND wc.IndicatorCode = wc2.IndicatorCode
+GROUP BY wc.CountryCode,wc.IndicatorCode;
+
+-- slope
+UPDATE worldbank_country_summary AS wu SET lr_percent_slope=(
+  SELECT ((2*lr_a+lr_b)-(lr_a+lr_b))/abs(lr_a+lr_b)*100 FROM worldbank_country_summary ws 
+  WHERE wu.CountryCode = ws.CountryCode AND wu.IndicatorCode = ws.IndicatorCode
+  );
+
+-- first value
+UPDATE worldbank_country_summary AS wu SET first_value=(
+  SELECT value FROM worldbank_country wc 
+  WHERE wu.CountryCode = wc.CountryCode AND wu.IndicatorCode = wc.IndicatorCode and wu.first_year = wc.year
+  );
+
+-- last value
+UPDATE worldbank_country_summary AS wu SET last_value=(
+  SELECT value FROM worldbank_country wc 
+  WHERE wu.CountryCode = wc.CountryCode AND wu.IndicatorCode = wc.IndicatorCode and wu.last_year = wc.year
+  );
+
+-- Percent range
+UPDATE worldbank_country_summary AS wu SET growth_percent=(
+  SELECT (last_value - first_value) / ABS(first_value)*100 FROM worldbank_country wc 
+  WHERE wu.CountryCode = wc.CountryCode AND wu.IndicatorCode = wc.IndicatorCode and wu.last_year = wc.year
+  );
+
+
+
+
 --------------------------------------
--- city view
+-- view
 --------------------------------------
 
-DROP VIEW IF EXISTS v_worldbank_country;
-CREATE VIEW IF NOT EXISTS v_geonames_city
+DROP VIEW IF EXISTS v_worldbank_country_summary;
+CREATE VIEW IF NOT EXISTS v_worldbank_country_summary
 AS
-SELECT
-  *
-FROM geonames_allcuntries
-WHERE geolevel='city';
+SELECT wi.IndicatorCode,category,description,CountryCode,first_year,last_year,nbyears,first_value, last_value, growth_percent, min_value, max_value, lr_a, lr_b, lr_percent_slope,data_quality,corr 
+FROM worldbank_indicator wi
+INNER JOIN worldbank_category wc ON wc.CATID=wi.CATID
+INNER JOIN worldbank_country_summary ws ON wi.IndicatorCode = ws.IndicatorCode
+WHERE wi.lang='FR'
+ORDER BY category,wi.IndicatorCode;
 
+--------------------------------------
+-- Database summary 
+--------------------------------------
+
+CREATE TABLE IF NOT EXISTS db_summary (
+  sourcename TEXT,
+  description TEXT,
+  value INTEGER,
+
+  PRIMARY KEY (sourcename,description)
+);
+
+REPLACE INTO db_summary VALUES('worldbank','nb distinct indicators',(select  count(DISTINCT IndicatorCode) from worldbank_country_summary));
+REPLACE INTO db_summary VALUES('worldbank','nb distinct french indicators',(SELECT count(*) FROM worldbank_indicator));
+REPLACE INTO db_summary VALUES('worldbank','nb indicators values',(SELECT count(*) FROM worldbank_country));
 COMMIT;
-
--- SELECT 
--- 'allperiods' as summary_type,
--- 'all' as period,
--- 'min' as sumary,
--- CountryCode, 
--- IndicatorCode, 
--- min(value) 
--- FROM worldbank_country
--- GROUP BY CountryCode,IndicatorCode; 
-
--- SELECT 
--- 'allperiods' as summary_type,
--- 'all' as period,
--- 'max' as sumary,
--- CountryCode, 
--- IndicatorCode, 
--- max(value) 
--- FROM worldbank_country
--- GROUP BY CountryCode,IndicatorCode; 
-
--- SELECT 
--- 'decades' as summary_type,
--- year - (year % 10),
--- 'min' as sumary,
--- CountryCode, 
--- IndicatorCode, 
--- min(value) 
--- FROM worldbank_country
--- GROUP BY CountryCode,IndicatorCode,year - (year % 10) 
--- ORDER by CountryCode,IndicatorCode,year;
-
--- SELECT wc.CountryCode, wc.IndicatorCode, MIN(wc.year) year_min, wcmin.value value_min, MAX(wc.year) yaer_max, wcmin.value value_max from worldbank_country wc 
--- INNER JOIN worldbank_country wcmin ON (wc.CountryCode = wcmin.CountryCode AND wc.IndicatorCode = wcmin.IndicatorCode AND wcmin.year = MIN(wc.year))
--- INNER JOIN worldbank_country wcmax ON (wc.CountryCode = wcmax.CountryCode AND wc.IndicatorCode = wcmax.IndicatorCode AND wcmax.year = MAX(wc.year))
--- GROUP BY wc.CountryCode,wc.IndicatorCode;
-
-
-----
--- SELECT *,min(year),max(year),min(value),max(value) 
--- FROM worldbank_country
--- GROUP BY CountryCode,IndicatorCode,category,year_decade  LIMIT 10;
-
