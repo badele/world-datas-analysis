32a544ba28fa21a05d646dbfe6ca4b928ca181fc
Bruno Adelé <brunoadele@gmail.com>
2021-09-28 08:34:50 +0200

    Importer refactoring

---
+++ dataset/init.sql
@@ -0,0 +1,81 @@
+.head on
+.nullvalue NULL
+
+--------------------------------------
+-- Dataset
+--------------------------------------
+
+--DROP TABLE IF EXISTS db_dataset;
+CREATE TABLE IF NOT EXISTS db_dataset(
+    SOURCE_ID	INTEGER,
+    Provider	    TEXT,
+    Source	    TEXT,
+    Licence       TEXT,
+    PRIMARY KEY(SOURCE_ID)
+);  
+
+--DROP TABLE IF EXISTS db_category;
+CREATE TABLE IF NOT EXISTS db_category(
+    CAT_ID              INTEGER,
+    SOURCE_ID	        INTEGER,
+    LANG                TEXT,
+    CategoryKey	        INTEGER,
+    Description	        TEXT,
+    PRIMARY KEY(CAT_ID AUTOINCREMENT)
+);  
+
+--DROP TABLE IF EXISTS db_indicator_description;
+CREATE TABLE IF NOT EXISTS db_indicator_description(
+    SOURCE_ID	            TEXT,
+    LANG                    TEXT,
+    IndicatorKey            TEXT,
+    Description	            TEXT,
+    PRIMARY KEY(SOURCE_ID, LANG, IndicatorKey) 
+);
+
+--DROP TABLE IF EXISTS db_indicator_in_category;
+CREATE TABLE IF NOT EXISTS db_indicator_in_category(
+    CAT_ID          INTEGER,
+    IndicatorKey    INTEGER,
+    PRIMARY KEY(CAT_ID, IndicatorKey) 
+);  
+
+--DROP TABLE IF EXISTS db_historical_country_value;
+CREATE TABLE IF NOT EXISTS db_historical_country_value(
+    SOURCE_ID   INTEGER,
+    COUNTRYCODE   TEXT,
+    YEAR          INTEGER,
+    IndicatorKey  TEXT,
+    value         REAL,
+    PRIMARY KEY(SOURCE_ID, YEAR, COUNTRYCODE, IndicatorKey) 
+);  
+CREATE INDEX IF NOT EXISTS idx_historical_sourceid ON db_historical_country_value(SOURCE_ID,YEAR);
+CREATE INDEX IF NOT EXISTS idx_historical_country_value_year ON db_historical_country_value(SOURCE_ID,YEAR);
+CREATE INDEX IF NOT EXISTS idx_historical_country_value_sourceid_countrycode_indicatorcode_year ON db_historical_country_value(SOURCE_ID,COUNTRYCODE,IndicatorKey,YEAR);
+CREATE INDEX IF NOT EXISTS idx_historical_country_value_sourceid_countrycode_indicatorcode ON db_historical_country_value(SOURCE_ID,COUNTRYCODE,IndicatorKey);
+
+
+--DROP TABLE IF EXISTS db_historical_country_summary;
+CREATE TABLE IF NOT EXISTS db_historical_country_summary (
+  SOURCE_ID INTEGER,
+  COUNTRYCODE TEXT,
+  IndicatorKey TEXT,
+  first_year INTEGER,
+  last_year INTEGER,
+  nbyears INTEGER,
+  first_value REAL,
+  last_value REAL,
+  growth_percent REAL,
+  min_value REAL,
+  max_value REAL,
+  lr_a REAL,
+  lr_b REAL,
+  lr_percent_slope REAL,
+  data_quality REAL,
+  corr REAL,
+  
+  PRIMARY KEY (SOURCE_ID, COUNTRYCODE,IndicatorKey)
+);
+
+CREATE INDEX IF NOT EXISTS idx_historical_country_summary_sourceid ON db_historical_country_summary(SOURCE_ID);
+CREATE INDEX IF NOT EXISTS idx_historical_country_summary_sourceid_countrycode_indicatorcode_firstyear ON db_historical_country_summary(SOURCE_ID,COUNTRYCODE,IndicatorKey,first_year);
--- lib/python/net.py
+++ lib/python/net.py
@@ -2,41 +2,61 @@
 
 import os
 import requests
+
+from tqdm import tqdm
 from ftplib import FTP
 
+MAX_RETRIES = 5
+CHUNK_SIZE = 8192
 
-# ftp = FTP('your-ftp-domain-or-ip')
-# ftp.login('your-username','your-password')
+# Source: https://github.com/owid/importers
+def downloadStreamFile(
+    url, filename, max_retries: int = MAX_RETRIES, bytes_read: int = None, verbose = False
+) -> bytes:
+    if bytes_read:
+        headers = {"Range": f"bytes={bytes_read}-"}
+    else:
+        headers = {}
+        bytes_read = 0
 
-# # Get All Files
-# files = ftp.nlst()
+    content = b""
+    try:
+        with requests.get(url, headers=headers, stream=True) as r:
+            r.raise_for_status()
+            for chunk in tqdm(r.iter_content(chunk_size=CHUNK_SIZE)):
+                bytes_read += CHUNK_SIZE
+                content += chunk
+    except requests.exceptions.ChunkedEncodingError:
+        if max_retries > 0:
+            if verbose:
+                print(f"Resuming downloading, remaining retry={max_retries-1}")
+            content += downloadStreamFile(url, filename, max_retries - 1, bytes_read)
+        else:
+            print.info("Download error")
 
-# # Print out the files
-# for file in files:
-# 	print("Downloading..." + file)
-# 	ftp.retrbinary("RETR " + file ,open("download/to/your/directory/" + file, 'wb').write)
+    return content
 
-# ftp.close()
+# Download Streamed http file
+def downloadStreamedHttpFile(url, filename, verbose=False):
+    if verbose:
+        print(f"Download {url}")
+    
+    content = downloadStreamFile(url, filename)
+    with open(filename, "wb") as f:
+        f.write(content)
 
-# end = datetime.now()
-# diff = end - start
-# print('All files downloaded for ' + str(diff.seconds) + 's')`
 
-
-
-def downloadHttpFile(url,filename,messagz=None):
-    if not os.path.exists(filename):
-        if messagz is not None:
-            print(messagz)
+def downloadHttpFile(url, filename, verbose=False):
+    if verbose:
+        print(f"Download {url}")
         
-        r = requests.get(url, allow_redirects=True)
-        with open(filename, 'wb') as f:
-            f.write(r.content)
+    r = requests.get(url, allow_redirects=True)
+    with open(filename, 'wb') as f:
+        f.write(r.content)    
 
-def downloadFtpFile(domaine,ftpfile,filename,username=None,password=None):
-    if not os.path.exists(filename):
-        ftp = FTP(domaine)
-        ftp.login(username,password)
+def downloadFtpFile(domaine, ftpfile, filename, username=None, password=None):
+    ftp = FTP(domaine)
+    ftp.login(username, password)
 
-        with open(filename, 'wb') as f:
-            ftp.retrbinary(f"RETR {ftpfile}" ,f.write)
+    with open(filename, "wb") as f:
+        ftp.retrbinary(f"RETR {ftpfile}", f.write)
