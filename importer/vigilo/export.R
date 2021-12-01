d8b0a7388d4cc3cff8bbdc5dc9495999e1758d6a
Bruno Adelé <brunoadele@gmail.com>
2021-12-01 19:06:07 +0100

    Update vigilo datas

--- importer/vigilo/export.R
+++ importer/vigilo/export.R
@@ -1,6 +1,8 @@
 suppressPackageStartupMessages({
   library(here)
+  library(dplyr)
   library(RSQLite)
+  library(lubridate)
 })
 
 scriptdir <- here()
@@ -12,9 +14,27 @@ source(paste0(scriptdir,"/lib/R/file.R"))
 # Make image directory
 dir.create(exportdir,recursive=TRUE, showWarnings = FALSE)
 
-# Get query
+
+# Init connexion
 con <- dbConnect(RSQLite::SQLite(), sqldb)
-data <- dbGetQuery(con, "SELECT name as instanceName, country, token, coordinates_lat, coordinates_lon, address, timestamp,cityname,catname  FROM v_vigilo_approved_observations")
+
+# Category
+category <- dbGetQuery(con, "SELECT CategoryId as catid, catname, catname_en, catcolor, catdisable FROM vigilo_category")
+write_fwf(category,paste0(exportdir,"categories.txt"))
+
+# Instance
+instance <- dbGetQuery(con, "SELECT InstanceId as instanceid, scope, name as instancename, api_path, country, version FROM vigilo_instance")
+write_fwf(instance,paste0(exportdir,"instances.txt"))
+
+obs <- dbGetQuery(con, "SELECT scope, token, coordinates_lat, coordinates_lon, address, timestamp,cityname,CategoryId,approved  FROM vigilo_observation vo INNER JOIN vigilo_instance vi ON vo.InstanceID = vi.InstanceID")
+
+obs <- obs %>%
+  mutate(
+    datetime = as.character(as_datetime(timestamp,origin="1970-01-01"))
+  ) %>%
+  relocate(datetime,.before = scope) %>%
+  arrange(datetime) %>%
+  select(-timestamp)
 
 # Export to Fixed with file
-write_fwf(data,paste0(exportdir,"approved_observations.txt"))
+write_fwf(obs,paste0(exportdir,"observations.txt"))
